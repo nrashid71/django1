1a14342cc4631462c78f08effedd313efebfd216
Rashid <nrashid@inbox.ru>
2020-03-05 18:42:47 +0300

    View details

--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -5,3 +5,6 @@ from .models import Article, Comment
 def index(request):
     latest_articles_list = Article.objects.order_by('-pub_date')[:5]
     return render(request, 'articles/list.html', {'latest_articles_list' : latest_articles_list})
+
+def detail(request, article_id):
+    pass
