2ce3ec86d08e12718c9f4539d469101d2303e142
Rashid <nrashid@inbox.ru>
2020-03-05 19:29:12 +0300

    done!!!

--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -2,7 +2,9 @@ from django.http import Http404, HttpResponseRedirect
 
 from django.shortcuts import render
 
-from .models import Article, Comment
+from django.urls import reverse
+
+from .models import Article
 
 def index(request):
     latest_articles_list = Article.objects.order_by('-pub_date')[:5]
@@ -14,7 +16,16 @@ def detail(request, article_id):
     except:
         raise Http404("Статья не найдена!")
     
-    return render(request, 'articles/detail.html', {'article': a})
+    latest_comment_list = a.comment_set.order_by('-id')[:10]
+
+    return render(request, 'articles/detail.html', {'article': a, 'latest_comment_list': latest_comment_list})
 
 def leave_comment(request, article_id):
-    pass
+    try:
+        a = Article.objects.get( id = article_id)
+    except:
+        raise Http404("Статья не найдена!")
+    
+    a.comment_set.create(author_name = request.POST['name'], comment_text = request.POST['text'])
+
+    return HttpResponseRedirect(reverse('articles:detail', args = (a.id,)))
