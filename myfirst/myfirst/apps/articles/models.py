9e93f0a9f6e757dde6eab4d9e34c2d9004423165
Rashid <nrashid@inbox.ru>
2020-03-05 17:28:46 +0300

    Russian our models

--- myfirst/myfirst/apps/articles/models.py
+++ myfirst/myfirst/apps/articles/models.py
@@ -14,6 +14,10 @@ class Article(models.Model):
     def was_published_recently(self):
         return self.pub_date >= (timezone.now() - datetime.timedelta(days = 7))
 
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete = models.CASCADE)
     author_name = models.CharField('имя автора', max_length = 50)
@@ -21,3 +25,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.author_name
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
