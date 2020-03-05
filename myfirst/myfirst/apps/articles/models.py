d989ae784aa6d86a7bd6da5449658cb467774ce5
Rashid <nrashid@inbox.ru>
2020-03-05 14:47:05 +0300

    Article and Comment models creating

--- myfirst/myfirst/apps/articles/models.py
+++ myfirst/myfirst/apps/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    article_title = models.CharField('название статьи', max_length = 200)
+    article_text = models.TextField('текст статьи')
+    pub_date = models.DateTimeField('дата публикации')
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete = models.CASCADE)
+    author_name = models.CharField('имя автора', max_length = 50)
+    comment_text = models.CharField('текст комментария', max_length = 200)
